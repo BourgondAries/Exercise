05534ba70a9f4cda2cd0cb1eb0c7771f1872775c
Kevin Robert Stravers <macocio@gmail.com>
2017-09-11 00:26:34 +0200

    Prevent double culling

--- word-iddfs.rkt
+++ word-iddfs.rkt
@@ -5,7 +5,7 @@
 
 (provide iddfs)
 
-(require lens levenshtein threading)
+(require "logger.rkt" lens levenshtein threading)
 
 (define all-words
   (with-input-from-file "/usr/share/dict/words"
@@ -18,32 +18,38 @@
 
 (define (find-single-distance-words dictionary word)
   (~>>
-    (filter (lambda (x) (= (string-length x) (string-length word))) dictionary)
-    (map (lambda (x) (list x (string-levenshtein x word))))
+    ; (filter (lambda (x) (= (string-length x) (string-length word))) dictionary)
+    (map (lambda (x) (list x (string-levenshtein x word))) dictionary)
     (filter (lambda (x) (= (second x) 1)))
     (map first)))
 
-(define (find-new-single-distance-words seen word)
-  (let* ([expanded (remove* seen (find-single-distance-words all-words word))]
+(define (find-new-single-distance-words seen word dictionary)
+  (let* ([expanded (remove* seen (find-single-distance-words dictionary word))]
          [seen* (remove-duplicates (append seen expanded))])
     (values expanded seen*)))
 
-(define (iddfs* i n seen from to)
+(define (iddfs* i n seen from to dictionary)
   (if (>= i n)
     (values seen #f)
-    (let-values ([(e* s*) (find-new-single-distance-words seen from)])
+    (let-values ([(e* s*) (find-new-single-distance-words seen from dictionary)])
       (if (member to e*)
         (values (list from to) #t)
         (let-values ([(s t) (for/fold ([s s*] [stop #f])
                                       ([e e*])
                                       #:break stop
-                                      (iddfs* (add1 i) n s e to))])
+                                      (iddfs* (add1 i) n s e to dictionary))])
           (values (cons from s) t))))))
 
 (define (iddfs from to)
-  (for/fold ([s '()] [stop #f])
-            ([n (in-naturals)])
-            #:break stop
-    (iddfs* 0 (add1 n) '() from to)))
+  (if (not (= (string-length from) (string-length to)))
+    #f
+    (let ([dictionary (filter (lambda (x) (= (string-length x) (string-length from))) all-words)])
+      (if (or (not (member from dictionary)) (not (member to dictionary)))
+        "Not in dict"
+        (for/fold ([s '()] [stop #f])
+                  ([n (in-naturals)])
+                  #:break stop
+          (info n)
+          (iddfs* 0 (add1 n) '() from to dictionary))))))
 
-(time (iddfs "sick" "well"))
+(time (iddfs "clean" "words"))
